315b197b5b2c3716b9cafcc5cb0c25a6e1d1018e
Tamil Selvan <tamil@tamilAir.aganitha.ai>
2023-03-28 18:54:16 +0530

    A working version

---
+++ atk_youtube_transcript/prompt.py
@@ -0,0 +1,10 @@
+title_prompt: str = """You are a youtube transcript punctuator. Given a youtube transcript, you should add all the punctuations wherever necessary without losing the context.
+                 Below is the transcript from a youtube video.
+                 Punctuate it and give it back in html format.
+
+                 The first line is the title. Add two new lines after the heading and make the title bold and then punctuate it."""
+
+general_prompt: str = """You are a youtube transcript punctuator. Given a youtube transcript, you should add all the punctuations wherever necessary without losing the context.
+                 Separate the transcripts into paragraphs wherever you think is logical and add titles to those paragraphs
+                 Below is the transcript from a youtube video.
+                 Punctuate it and give it back in html format."""
--- atk_youtube_transcript/transcript.py
+++ atk_youtube_transcript/transcript.py
@@ -1,29 +1,38 @@
 import requests
+from typing import List
 from atk_youtube_transcript.youtube import YouTube
 from atk_youtube_transcript.utils import data
+from langchain.docstore.document import Document
 import typer
-from langchain.chat_models import ChatOpenAI
+from langchain.text_splitter import TokenTextSplitter
+from langchain.llms import OpenAI
+from atk_youtube_transcript.prompt import title_prompt, general_prompt
+import logging
+import traceback
+import sys
+from langchain.document_loaders import YoutubeLoader
+import warnings
 
 
+warnings.filterwarnings("ignore")
+logging.basicConfig(level='INFO')
 app = typer.Typer()
 
 
 @app.command()
 def transcript(video_code: str, outfile: str):
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0)
     idx = 0
-    prompt: str = """You are a youtube transcript punctuator. Given a youtube transcript, you should add all the punctuations wherever necessary without losing the context.
-    Below is the transcript from a youtube video.
-    Punctuate it and give it back in html format.
-
-    The first line is the title. Add two new lines after the heading and make the title bold and then punctuate it."""
-    youtube = YouTube(video_code)
+    prompt: str = title_prompt
+    youtube: YouTube = YouTube(video_code)
 
     api_chapters_response: requests.Response = requests.get(f'https://yt.lemnoslife.com/videos?part='
                                                             f'chapters&id={video_code}')
-    
-    if api_chapters_response.ok:
-        parsed_time, parsed_title, parsed_images_url = youtube.from_chapters(api_chapters_response)
+
+    parsed_time, parsed_title, parsed_images_url = youtube.from_chapters(api_chapters_response)
+    if len(parsed_time) != 0:
+        logging.info(f"There are total {len(parsed_title)} chapters")
+        logging.info(f"Usually it takes around 5-8 mins for a 40 mins video")
         start, text = data(parsed_time=parsed_time, parsed_title=parsed_title, video_code=video_code, method="chapters")
         counter = 0
         for i in range(len(text)):
@@ -31,9 +40,7 @@ def transcript(video_code: str, outfile: str):
                 chunked_text = text[idx:i]
                 chunk = " ".join(chunked_text)
                 new_prompt = prompt + "\n\n" + chunk
-                print(new_prompt)
                 response: str = llm(new_prompt)
-                print(response)
                 with open(str(outfile)+".html", "a+") as file:
                     file.write(f"\n\n{response}")
                     file.write(f"\n<img src=\"{parsed_images_url[counter]}\" alt=\"Not Applicable\" />")
@@ -41,27 +48,55 @@ def transcript(video_code: str, outfile: str):
                                f"{chunked_text[0]}</a>\n")
                     counter += 1
                 idx = i+1
+                logging.info(f"Punctuated transcription completed for chapters: {counter}")
+        return
     else:
         api_description_response: requests.Response = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part='
                                                                    f'snippet&id={video_code}&'
                                                                    f'key=AIzaSyDQP56aoFMwjJsanu3dfiGXmQEIkb-BzLc')
-        if api_description_response.ok:
-            parsed_time, parsed_title = youtube.from_description(api_description_response)
-            start, text = data(parsed_time=parsed_time, parsed_title=parsed_title, video_code=video_code, method="description")
 
+        parsed_time, parsed_title = youtube.from_description(api_description_response)
+        if len(parsed_time) != 0:
+            logging.info(f"There are total {len(parsed_title)} titles")
+            logging.info(f"Usually it takes around 5-8 mins for a 40 mins video")
+            start, text = data(parsed_time=parsed_time, parsed_title=parsed_title, video_code=video_code, method="description")
+            counter = 0
             for i in range(len(text)):
                 if text[i] == "\n\n\n":
                     chunked_text = text[idx:i]
                     chunk = " ".join(chunked_text)
                     new_prompt = prompt + "\n\n" + chunk
-                    print(new_prompt)
                     response: str = llm(new_prompt)
-                    print(response)
                     with open(str(outfile)+".html", "a+") as file:
                         file.write(f"\n\n{response}")
                         file.write(f"\n<a href=\"https://www.youtube.com/watch?v={video_code}&t="
                                    f"{start[idx]}s\">{chunked_text[0]}</a>\n")
+                    counter += 1
                     idx = i+1
+                    logging.info(f"Punctuated transcription completed for chapters: {counter}")
+            return
+        else:
+            try:
+                logging.info("Your video don't contain chapters or titles in the description."
+                             "So ChatGPT itself is going to add titles for your video")
+                logging.info(f"Usually it takes around 5-8 mins for a 40 mins video")
+                loader = YoutubeLoader.from_youtube_channel(f"https://www.youtube.com/watch?v={video_code}")
+                transcripts: List[Document] = loader.load()
+                doc: Document = transcripts[0]
+                text_splitter = TokenTextSplitter(
+                    chunk_size=1900, chunk_overlap=0)
+                texts = text_splitter.split_text(doc.page_content)
+                docs = [Document(page_content=t) for t in texts]
+                for d in docs:
+                    prompt: str = general_prompt + "\n\n" + \
+                                  d.page_content.replace('[Music]', '')
+                    response: str = llm(prompt)
+                    with open(outfile+".html", 'a+') as f:
+                        f.write(f"\n{response}")
+                return
+            except Exception as err:
+                traceback.print_exc(file=sys.stdout)
+                return
 
 
 def main() -> None:
